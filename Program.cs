01868b4620884931ac84e51ec10ba32ab677e45a
henriqueweege <henriqueweege@gmail.com>
2023-10-06 18:17:15 -0300

    finalizando desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+public class Program
+{
+   static void Main() 
+   {
+        string numero = "numero"; 
+        string modelo = "modelo";
+        string imei = "imei";
+        int memoria = 1;
+
+        var nokia = new Nokia(numero, modelo, imei, memoria);
+        nokia.InstalarAplicativo("");
+
+        Console.WriteLine($"nokia numero {nokia.Numero}");   
+        Console.WriteLine($"nokia modelo {nokia.Modelo}");   
+        Console.WriteLine($"nokia IMEI {nokia.IMEI}");   
+        Console.WriteLine($"nokia memoria {nokia.Memoria}");   
+
+
+        var iphone = new Iphone(numero, modelo, imei, memoria);
+        iphone.InstalarAplicativo("");
+
+        Console.WriteLine($"iphone numero {nokia.Numero}");   
+        Console.WriteLine($"iphone modelo {nokia.Modelo}");   
+        Console.WriteLine($"iphone IMEI {nokia.IMEI}");   
+        Console.WriteLine($"iphone memoria {nokia.Memoria}");
+   }
+}
